aad30c2b5e5fae4ec858a39cbf7dc35d5147d4ff
10041 <andreyoleksyuk004@gmail.com>
2017-10-06 12:43:24 +0300

    add task05

--- task05.js
+++ task05.js
@@ -1,13 +1,77 @@
 let fs = require('fs');
+let path = require(`path`);
 if(!fs.existsSync(process.argv[2])){
     console.log("Неправильный путь");
     return;
 }
-fs.open(process.argv[2]+"\\summary.js", `w+`, 0644, (err, file_handle) => {
-    if(!err){
-        console.log("ОК");
-    }
-    else{
-        console.log("Ошибка при открытии!");
-    }
-});
+if (!fs.existsSync("./config.json")) {
+	console.log("Конфига нет!");
+	return;
+}
+let copyrightStr = JSON.parse(fs.readFileSync("./config.json")).copyright;
+
+let scriptStr = "let fs = require(`fs`);\n\
+let path = require(`path`);\n\
+const ListPath = (inputPath, folder, copyrightParam) => {\n\
+    fs.readdir(inputPath, (err, files)=>{\n\
+        files.forEach(function(file) {\n\
+            if(file != 'Summary'){\n\
+                let filePath = `${inputPath}\\\\${file}`;\n\
+                fs.stat(filePath, (err, stats)=>{\n\
+                    if(stats.isDirectory()){\n\
+                        folder == '' ? ListPath(filePath, file, copyrightParam) : ListPath(filePath, `${folder}/${file}`, copyrightParam);\n\
+                    }\n\
+                    else\n\
+                    {\n\
+                        if(path.extname(`${folder}\\${file}`) === '.txt')\n\
+                        {\n\
+                            let newFile = `${summaryPath}\\\\${file}`;\n\
+                            fs.writeFileSync(newFile, '');\n\
+                            fs.appendFile(newFile, `${copyrightParam}`, (err)=>{\n\
+                                if(!err){\n\
+                                    fs.readFile(filePath, (err, data) => {\n\
+                                        if(!err){\n\
+                                            fs.appendFile(newFile, data, (err)=>{\n\
+                                                if(!err){\n\
+                                                    fs.appendFile(newFile, `${copyrightParam}`, (err) =>{\n\
+                                                        if(err){\n\
+                                                            console.log('Ошибка записи в файл');\n\
+                                                            return;\n\
+                                                        }\n\
+                                                    })\n\
+                                                }\n\
+                                            })\n\
+                                        }\n\
+                                        else{\n\
+                                            console.log('Ошибка чтения файла');\n\
+                                            return;\n\
+                                        }\n\
+                                    })\n\
+                                }\n\
+                                else{\n\
+                                    console.log('Ошибка записи в файл');\n\
+                                    return;\n\
+                                }\n\
+                            })\n\
+                        }\n\
+                    }\n\
+                    folder == '' ? console.log(file) : console.log(`${folder}/${file}`);\n\
+                })\n\
+            }\n\
+        }, this);\n\
+    })\n\
+}\n\
+let summaryPath = '"+process.argv[2]+"\\\\Summary';\n\
+if(!fs.existsSync(summaryPath))\n\
+{\n\
+    fs.mkdirSync(summaryPath)\n\
+}\n\
+\n\
+ListPath('"+process.argv[2]+"', '', '"+copyrightStr+"');\n\
+setTimeout(()=>{\n\
+    fs.watch(summaryPath, (eventType, filename) =>{\n\
+        console.log(`${new Date()} ${eventType} - ${filename}`)\n\
+    })\n\
+}, 1000);"
+
+fs.writeFileSync("summary.js", scriptStr);
