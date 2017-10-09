27507f28f01cdd1371936396f1f0aa7833120233
10041 <andreyoleksyuk004@gmail.com>
2017-10-09 10:04:18 +0300

    fix reduce

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let result_str = process.argv.reduce((outRes, value, index)=>{
-    return index > 1 ? (outRes += `${value}, `) : outRes;
-}, "");
 
-console.log(`Hi param: ${result_str}`);
+let result_str = process.argv.reduce((previousValue, currentValue, index)=>{
+    return index > 1 ? (previousValue += `${currentValue}, `) : previousValue;
+}, "Hi param: ");
+
+console.log(result_str);
